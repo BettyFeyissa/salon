947fcd6633ff004ae355785c182f46db8b1653a4
bettyfeyissa <bettyfeyissa@gmail.com>
2022-11-11 11:27:09 -0600

    we declare the variables and created the constructor

--- src/src/Salon.java
+++ src/src/Salon.java
@@ -1,2 +1,14 @@
-public class Salon {
+public abstract class Salon {
+   public String name;
+   public String description;
+   public   String appointment;
+   public double price;
+
+   //create constructor
+    public Salon(String name, String description, String appointment, double price) {
+        this.name = name;
+        this.description = description;
+        this.appointment = appointment;
+        this.price = price;
+    }
 }
